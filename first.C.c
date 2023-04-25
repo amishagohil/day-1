8499542d43c4e6b4f157ae883ab2ae4452500fe4
amishagohil <131762811+amishagohil@users.noreply.github.com>
2023-04-25 22:56:46 +0530

    day 1

---
+++ first.C.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("amisha gohil\n20\ndiamond\n");	
+	printf(" --------\n");
+	printf("|        |\n");
+	printf("|        |\n");
+	printf("| amisha |\n");
+	printf("|        |\n");
+	printf("|        |\n");
+	printf(" --------\n");
+	printf("---------\n");
+	printf("|        |\n");
+	printf("A        |\n");
+	printf("M        |\n");
+	printf("S        |\n");
+	printf("H        |\n");
+	printf("A        |\n");
+	printf("|        |\n");
+	printf(" --------\n");
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("****\n");
+	printf("***\n");
+	printf("**\n");
+	printf("*\n\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*                                *  *\n");
+	printf("*                              *      *\n ");
+    printf("*        *   *              *           *\n"); 
+	printf("*      *       *          *   \n");
+	printf("*   *            *      *\n");
+	printf("* *               *  *\n");
+	printf("*\n");
+	printf("       *       * *   * *  * * * * *  * * * * *  *     *       *\n");
+	printf("     *   *     *   *   *      *      * * * * *  * * * *     *   *\n");
+	printf("    *  *   *   *       *      *              *  *     *    *   *  *\n");
+	printf("   *        *  *       *  * * * * *  * * * * *  *     *  *         *");
+	return 0;
+	
+}
